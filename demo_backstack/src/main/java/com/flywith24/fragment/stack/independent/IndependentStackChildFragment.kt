cd88a5d2456b12deb18f041f4616113e17d40860
Young Yunzhao <fly_with24@163.com>
2020-03-05 22:41:44 +0800

    重命名独立返回栈名称

--- demo_backstack/src/main/java/com/flywith24/fragment/stack/independent/IndependentStackChildFragment.kt
+++ demo_backstack/src/main/java/com/flywith24/fragment/stack/independent/IndependentStackChildFragment.kt
@@ -1,8 +1,9 @@
-package com.flywith24.fragment
+package com.flywith24.fragment.stack.independent
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.commit
+import com.flywith24.fragment.R
 import com.flywith24.fragment.databinding.FragmentChildBinding
 import com.flywith24.library.base.BaseFragment
 import com.flywith24.library.base.ext.args
@@ -11,11 +12,11 @@ import com.flywith24.library.base.ext.args
  * @author yyz (杨云召)
  * @date   2020/3/4
  * time   10:16
- * description
+ * description 独立返回栈 子fragment
  * childFragment 显示stack名字及fragment个数,
- * fragmentManager 共用[ParentFragment]的[getChildFragmentManager]
+ * fragmentManager 共用[IndependentStackParentFragment]的[getChildFragmentManager]
  */
-class ChildFragment : BaseFragment<FragmentChildBinding>(R.layout.fragment_child) {
+class IndependentStackChildFragment : BaseFragment<FragmentChildBinding>(R.layout.fragment_child) {
     override fun initBinding(view: View): FragmentChildBinding = FragmentChildBinding.bind(view)
 
     val stableTag: String
@@ -30,15 +31,21 @@ class ChildFragment : BaseFragment<FragmentChildBinding>(R.layout.fragment_child
         binding.button.text = getString(R.string.fragmentHint, name, depth)
         binding.button.setOnClickListener {
             parentFragmentManager.commit {
-                replace(id, newInstance(name, depth + 1))
+                replace(
+                    id,
+                    newInstance(
+                        name,
+                        depth + 1
+                    )
+                )
                 addToBackStack(id.toString())
             }
         }
     }
 
     companion object {
-        fun newInstance(name: String, depth: Int): ChildFragment =
-            ChildFragment().apply {
+        fun newInstance(name: String, depth: Int): IndependentStackChildFragment =
+            IndependentStackChildFragment().apply {
                 this.name = name
                 this.depth = depth
             }
--- demo_backstack/src/main/java/com/flywith24/fragment/stack/independent/IndependentStackParentFragment.kt
+++ demo_backstack/src/main/java/com/flywith24/fragment/stack/independent/IndependentStackParentFragment.kt
@@ -1,9 +1,10 @@
-package com.flywith24.fragment
+package com.flywith24.fragment.stack.independent
 
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.fragment.app.commit
+import com.flywith24.fragment.R
 import com.flywith24.fragment.databinding.FragmentParentBinding
 import com.flywith24.library.base.BaseFragment
 import com.flywith24.library.base.ext.addOnBackPressedCallback
@@ -12,10 +13,11 @@ import com.flywith24.library.base.ext.addOnBackPressedCallback
  * @author yyz (杨云召)
  * @date   2020/3/4
  * time   10:02
- * description
- * parentFragment 包含四个返回栈，各个返回栈中显示若干[ChildFragment]
+ * description  独立返回栈父fragment
+ * parentFragment 包含四个返回栈，各个返回栈中显示若干[IndependentStackChildFragment]
  */
-class ParentFragment : BaseFragment<FragmentParentBinding>(R.layout.fragment_parent) {
+class IndependentStackParentFragment :
+    BaseFragment<FragmentParentBinding>(R.layout.fragment_parent) {
     override fun initBinding(view: View): FragmentParentBinding = FragmentParentBinding.bind(view)
 
     override fun init(savedInstanceState: Bundle?) {
@@ -29,14 +31,23 @@ class ParentFragment : BaseFragment<FragmentParentBinding>(R.layout.fragment_par
         if (savedInstanceState == null) {
             for (containerId in containerIds) {
                 childFragmentManager.commit {
-                    val fragment = ChildFragment.newInstance(name(containerId), 1)
+                    val fragment =
+                        IndependentStackChildFragment.newInstance(
+                            name(containerId),
+                            1
+                        )
                     replace(containerId, fragment, fragment.stableTag)
                 }
             }
         }
     }
 
-    private val containerIds = intArrayOf(R.id.stack_1, R.id.stack_2, R.id.stack_3, R.id.stack_4)
+    private val containerIds = intArrayOf(
+        R.id.stack_1,
+        R.id.stack_2,
+        R.id.stack_3,
+        R.id.stack_4
+    )
     private fun name(@IdRes containerId: Int) =
         resources.getResourceEntryName(containerId).replace("_", " ")
 
