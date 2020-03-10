9d76ce268599e90d6beea9c25449ed1d5a011cc7
fly_with24 <fly_with24@163.com>
2020-03-10 15:11:07 +0800

    add viewpager2 demo

---
+++ demo_viewpager2/src/main/java/com/flywith24/demo_viewpager2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.flywith24.demo_viewpager2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
