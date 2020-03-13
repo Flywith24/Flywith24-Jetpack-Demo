e3361ce9656252dd326ee0bb02f70192b466739d
fly_with24 <fly_with24@163.com>
2020-03-13 15:43:08 +0800

    加入清空全部按钮

--- demo_backstack/src/main/java/com/flywith24/fragment/stack/multiple/MultipleStackChildFragment.kt
+++ demo_backstack/src/main/java/com/flywith24/fragment/stack/multiple/MultipleStackChildFragment.kt
@@ -5,13 +5,12 @@ import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
 import androidx.fragment.app.commit
 import com.flywith24.fragment.R
 import com.flywith24.fragment.databinding.FragmentMultipleChildBinding
 import com.flywith24.fragment.stack.common.BaseStackFragment
-import com.flywith24.library.base.ext.MutedColors
-import com.flywith24.library.base.ext.args
-import com.flywith24.library.base.ext.isDarkTheme
+import com.flywith24.library.base.ext.*
 
 /**
  * @author yyz (杨云召)
@@ -31,27 +30,44 @@ class MultipleStackChildFragment :
 
     override fun init(savedInstanceState: Bundle?) {
         with(binding.text) {
-            text = getString(R.string.fragmentHint, name, getCount(depth))
+            text = getString(R.string.fragmentHint).formatSpanned(
+                name,
+                getCount(depth),
+                getString(R.string.clear)
+                    .scale(0.4F)
+                    .color(requireContext().getColor(R.color.colorRed))
+                    .underline()
+                    .italic()
+                    .bold()
+                    .click {
+                        parentFragmentManager.popBackStack(name, POP_BACK_STACK_INCLUSIVE)
+                    },
+                getString(R.string.next)
+                    .scale(0.6F)
+                    .underline()
+                    .italic()
+                    .bold()
+                    .click {
+                        if (depth < 13) {
+                            parentFragmentManager.commit {
+                                addToBackStack(name)
+                                replace(R.id.content, newInstance(name, depth + 1))
+                            }
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                getString(R.string.done, name),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            )
             textSize = resources.getDimensionPixelSize(R.dimen.large_text).toFloat()
             movementMethod = LinkMovementMethod.getInstance()
             setTextColor(ContextCompat.getColor(requireContext(), R.color.colorWhite))
             setBackgroundColor(
                 MutedColors.colorAt(requireContext().isDarkTheme, depth)
             )
-            setOnClickListener {
-                if (depth < 13) {
-                    parentFragmentManager.commit {
-                        addToBackStack(name)
-                        replace(R.id.content, newInstance(name, depth + 1))
-                    }
-                } else {
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.done, name),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
         }
     }
 
