e7ddedca16c4358837ebbc057afc1896bc5f6dd7
fly_with24 <fly_with24@163.com>
2020-03-17 15:04:30 +0800

    add comment

--- demo_savestate/src/main/java/com/flywith24/demo_savestate/FirstFragment.kt
+++ demo_savestate/src/main/java/com/flywith24/demo_savestate/FirstFragment.kt
@@ -10,14 +10,31 @@ import androidx.fragment.app.replace
 import com.flywith24.demo_savestate.databinding.FragentFirstBinding
 import com.flywith24.library.base.BaseFragment
 
+/**
+ * @author yyz(杨云召)
+ * @date   2020/3/17
+ * time   14:51
+ * description
+ * 测试 replace 状态保存
+ * 1. 在 EditText 内输入内容， 输入的内容会存在 [label] 中并赋值给 TextView
+ * 2. 点击 click replace 该 fragment 测试
+ * 3. 旋转屏幕测试
+ * 结果：
+ * 步骤2 返回该 fragment 后 EditText 和 TextView 均恢复到之前的状态，[label] 有值
+ * 步骤3 旋转屏幕后 EditText 和 TextView 均恢复到之前的状态，[label] 无值
+ *
+ */
 class FirstFragment : BaseFragment<FragentFirstBinding>(R.layout.fragent_first) {
     override fun initBinding(view: View): FragentFirstBinding = FragentFirstBinding.bind(view)
     var label = ""
 
     override fun init(savedInstanceState: Bundle?) {
+        //打印成员状态
         Log.i(TAG, "init: $label")
+
         binding.edit.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
+                //为成员赋值
                 label = s.toString()
                 binding.text.text = label
             }
