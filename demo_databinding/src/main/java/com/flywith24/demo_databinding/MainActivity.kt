bdd6626c76caefbe917be3b055b57f11dfe49f8b
fly_with24 <fly_with24@163.com>
2020-04-03 11:39:36 +0800

    binding editText

--- demo_databinding/src/main/java/com/flywith24/demo_databinding/MainActivity.kt
+++ demo_databinding/src/main/java/com/flywith24/demo_databinding/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.flywith24.demo_databinding
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.flywith24.demo_databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private val myViewModel by viewModels<MyViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
+            lifecycleOwner = this@MainActivity
+            vm = myViewModel
+        }
     }
 }
