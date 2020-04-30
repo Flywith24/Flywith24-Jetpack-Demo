3a88119f7f1a9b8c7df399b9c5c892d5fcd823a7
fly_with24 <fly_with24@163.com>
2020-04-30 10:50:00 +0800

    add FragmentResult demo

--- demo_newapi/src/main/java/com/flywith24/demo_newapi/FragmentA.kt
+++ demo_newapi/src/main/java/com/flywith24/demo_newapi/FragmentA.kt
@@ -1,6 +1,14 @@
 package com.flywith24.demo_newapi
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
+import kotlinx.android.synthetic.main.fragment_common.*
 
 /**
  * @author yyz (杨云召)
@@ -8,6 +16,25 @@ import androidx.fragment.app.Fragment
  * time   9:48
  * description
  */
-class FragmentA : Fragment() {
+class FragmentA : Fragment(R.layout.fragment_common) {
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        root.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.colorAccent))
+        text.text = FragmentA::class.simpleName
+
+        button.setOnClickListener {
+
+            setFragmentResult(
+                REQUEST_KEY1, bundleOf(
+                    BUNDLE_KEY1 to "result",
+                    BUNDLE_KEY2 to "来自 FragmentA"
+                )
+            )
+        }
+        setFragmentResultListener(REQUEST_KEY2) { key, bundle ->
+            text.text = "${bundle.getString(BUNDLE_KEY1)}，${bundle.getString(BUNDLE_KEY2)}"
+        }
+    }
 
 }
--- demo_newapi/src/main/java/com/flywith24/demo_newapi/MainActivity.kt
+++ demo_newapi/src/main/java/com/flywith24/demo_newapi/MainActivity.kt
@@ -3,3 +3,9 @@ package com.flywith24.demo_newapi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
+
+const val REQUEST_KEY1 = "REQUEST_KEY1"
+const val REQUEST_KEY2 = "REQUEST_KEY2"
+
+const val BUNDLE_KEY1 = "BUNDLE_KEY1"
+const val BUNDLE_KEY2 = "BUNDLE_KEY2"
