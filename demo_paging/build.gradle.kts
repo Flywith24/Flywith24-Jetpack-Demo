734d0a27322362e916d1771a3c8f43d0b8f3fb53
fly_with24 <fly_with24@163.com>
2020-05-08 14:41:57 +0800

    add paging demo

---
+++ demo_paging/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application")
+    id("com.build.plugin")
+}
+android { defaultConfig { applicationId = "com.flywith24.demo_paging" } }
+
+dependencies {
+    implementation(project(":library_base"))
+}
---
+++ demo_paging/src/main/java/com/flywith24/demo_paging/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.flywith24.demo_paging
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+
+}
