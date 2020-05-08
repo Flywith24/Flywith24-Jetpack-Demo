bfd99b502ea706c2bad7babc6d28ba7e22a89a42
fly_with24 <fly_with24@163.com>
2020-05-08 15:00:37 +0800

    add retrofit dependency

--- demo_paging/build.gradle.kts
+++ demo_paging/build.gradle.kts
@@ -9,4 +9,6 @@ android { defaultConfig { applicationId = "com.flywith24.demo_paging" } }
 dependencies {
     implementation(project(":library_base"))
     implementation(AndroidX.paging)
+    implementation("com.squareup.retrofit2:retrofit:2.8.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.8.1")
 }
