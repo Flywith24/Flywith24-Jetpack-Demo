cb989e67bd06bee25298e8d847355495bb9653a9
Flywith24 <fly_with24@163.com>
2020-09-07 10:42:58 +0800

    use GradlePlugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
 
+buildscript {
     repositories {
         google()
         jcenter()
         
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
+
+        classpath(GradlePlugins.ANDROID)
+        classpath(kotlin(module = GradlePlugins.Kotlin.ID, version = GradlePlugins.Kotlin.VERSION))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
