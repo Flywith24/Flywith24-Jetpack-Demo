e2d023f5488590bac346f1652802f64f73a6025e
Flywith24 <fly_with24@163.com>
2020-09-07 10:42:30 +0800

    bump Kotlin to 1.4.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:3.6.3")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    implementation("com.android.tools.build:gradle:4.0.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
     implementation(gradleApi())
     implementation(localGroovy())
 }
