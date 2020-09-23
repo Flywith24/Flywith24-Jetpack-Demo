dc42a2f825f5751f4c0a699ac21f5ad86d644c1e
Flywith24 <fly_with24@163.com>
2020-09-23 15:36:09 +0800

    import hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ buildscript {
     repositories {
         google()
         jcenter()
-        
+
     }
     dependencies {
 
         classpath(GradlePlugins.ANDROID)
         classpath(kotlin(module = GradlePlugins.Kotlin.ID, version = GradlePlugins.Kotlin.VERSION))
-
+        classpath(Google.hiltPlugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -21,7 +21,7 @@ allprojects {
     repositories {
         google()
         jcenter()
-        
+
     }
 }
 
--- demo_hilt/build.gradle.kts
+++ demo_hilt/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("com.android.application")
     id("com.build.plugin")
+    id("dagger.hilt.android.plugin")
 }
 android {
-    defaultConfig { applicationId "com.flywith24.demo_hilt" }
+    defaultConfig { applicationId = "com.flywith24.demo_hilt" }
 
     dependencies {
         implementation(project(":library_base"))
+        implementation(Google.hilt)
+        kapt(Google.hiltCompiler)
     }
 }
