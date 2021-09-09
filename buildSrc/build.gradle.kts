7e1d15eb7a0f34dae35c253d0db06e851242b7c7
Flywith24 <fly_with24@163.com>
2021-09-09 10:45:19 +0800

    :wrench: bump BGP version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.0.1")
+    implementation("com.android.tools.build:gradle:7.0.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     implementation(gradleApi())
     implementation(localGroovy())
